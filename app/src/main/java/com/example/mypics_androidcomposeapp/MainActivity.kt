5a19afba65ab025aef58e21989cb57876ce7ccdc
Almaz Ermilov <almaz.ermilov@gmail.com>
2024-04-26 11:02:16 +0200

    add top app bar

--- app/src/main/java/com/example/mypics_androidcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mypics_androidcomposeapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mypics_androidcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,13 +13,19 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
@@ -63,7 +70,7 @@ fun AppNavigation() {
             val imageId = backStackEntry.arguments?.getInt("imageId")
             val viewModel: MainViewModel = viewModel() // Retrieve ViewModel in Composable scope
             viewModel.getImageById(imageId)?.let { image ->
-                ImageDetailScreen(image) // This is now correctly placed in a Composable function
+                ImageDetailScreen(image, navController) // This is now correctly placed in a Composable function
             } ?: run {
                 Text("Error: Image not found")
             }
@@ -71,11 +78,27 @@ fun AppNavigation() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel, navController: NavController) {
     Column {
+        TopAppBar(
+            title = { Text(
+                "Home screen",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+            },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = Color.LightGray
+            ),
+        )
         PlaceholderImagesList(viewModel)
+
         Text(text = "_____________________________________")
+
         SavedImagesList(viewModel)
     }
 }
@@ -147,10 +170,31 @@ fun PlaceholderImagesList(viewModel: MainViewModel) {
 
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ImageDetailScreen(image: ImageModel) {
+fun ImageDetailScreen(image: ImageModel, navController: NavController) {
     if (image != null) {
         Column {
+            TopAppBar(
+                title = {
+                    Row {
+                        Text("<", modifier = Modifier.clickable {
+                            navController.navigate("mainScreen")
+                        })
+
+                        Text(
+                            "Photos",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.LightGray
+                ),
+            )
+
         Text(text = "Selected Image")
         Spacer(modifier = Modifier.height(16.dp))
         AsyncImage(
